b1fe8bc7ab45bc68eb5a24f579559a01bd24ff93
tobiasfurtlehner <fur17466@spengergasse.at>
2021-12-02 15:52:24 +0100

    Added pandas implementation for day one

--- challenges/day_one.py
+++ challenges/day_one.py
@@ -1,4 +1,5 @@
 from helpers import read_input
+import pandas as pd
 
 def day_one_part_one(puzzle_input):
     incr = 0
@@ -6,6 +7,11 @@ def day_one_part_one(puzzle_input):
         if puzzle_input[i] > puzzle_input[i - 1]:
             incr += 1
     return incr
+
+def day_one_part_one_pandas(puzzle_input):
+    df = pd.DataFrame(puzzle_input)
+    df['incr'] = df[0].rolling(2).apply(lambda arr: arr[0] < arr[1], raw=True)
+    return int(df['incr'].sum())
 
 def day_one_part_two(puzzle_input):
     result = 0
@@ -16,7 +22,14 @@ def day_one_part_two(puzzle_input):
             result += 1
     return result
 
+def day_one_part_two_pandas(puzzle_input):
+    df = pd.DataFrame(puzzle_input)
+    df['rolling'] = df[0].rolling(3).sum()
+    df['incr'] = df['rolling'].rolling(2).apply(lambda arr: arr[0] < arr[1], raw=True)
+    return int(df['incr'].sum())
+
 if __name__ == '__main__':
+
     items = read_input('day_one.txt', parse_int=True)
     print('Day One:')
     print(f'-- Part One: {day_one_part_one(items)}')
